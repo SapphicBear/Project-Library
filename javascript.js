c050acd10948c973afba51b6bb53bc5d568ad5ba
SapphicBear <235136735+SapphicBear@users.noreply.github.com>
2025-11-21 19:05:50 +0100

    Add initial scaffholding for project
    Add initial code and styling for the project

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// Create unique id for the books using crypto.randomUUID().
+// Make constructor for book objects to be populated with values and information
+// Populate the 'myLibrary' array with those objects and use a loop to display them on the DOM.
+// Add a 'New book button' that allows the user to add a new book using the constructor function
+// Add this new book onto the array and thusly the DOM.
+// Add a 'remove' button to remove a book from the array and the DOM
+// Add a button that marks the book as 'read'.
+const bookshelf = document.querySelector(".bookshelf");
+const bookCard = document.createElement("div").classList = "book-card";
+
+const myLibrary = [];
